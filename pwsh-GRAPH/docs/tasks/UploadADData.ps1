980da214592b4d635224626fc8e615d407d17bb4
Rui Nogueira <ruinogueira@tuta.io>
2023-05-17 15:04:44 +0000

    update

--- pwsh-GRAPH/docs/tasks/UploadADData.ps1
+++ pwsh-GRAPH/docs/tasks/UploadADData.ps1
@@ -1,7 +1,5 @@
-$actions = (New-ScheduledTaskAction -Execute 'C:\dev\pwsh-edu-graph\src\private\Scripts\UploadDadosAD.ps1')
+$actions = (New-ScheduledTaskAction -Execute 'UploadADData')
 $trigger = New-ScheduledTaskTrigger -Daily -At '11:00 PM'
-$principal = New-ScheduledTaskPrincipal -UserId 'edu\sysadmin' -RunLevel Highest
+$principal = New-ScheduledTaskPrincipal -UserId 'domain\service-account' -RunLevel Highest
 $settings = New-ScheduledTaskSettingsSet -WakeToRun
 $task = New-ScheduledTask -Action $actions -Principal $principal -Trigger $trigger -Settings $settings
-
-l
