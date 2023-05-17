b8802f8adc97498471fbf23e8677f8b3f8def97a
Rui Nogueira <ruinogueira@tuta.io>
2023-05-17 15:08:10 +0000

    update

--- pwsh-GRAPH/src/public/public.psm1
+++ pwsh-GRAPH/src/public/public.psm1
@@ -1,4 +1,4 @@
-using module "$PSScriptRoot\\pwsh-Graph\src\models\ConnectionHandler.psm1"
+using module "./src/models/ConnectionHandler.psm1"
 
 Write-Verbose 'A carregar as funções públicas'
 . $PSScriptRoot\Get-OneDriveInfo.ps1
