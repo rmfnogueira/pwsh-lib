c944fad9b4103f97007946a85462d0421831936b
Rui Nogueira <ruinogueira@tuta.io>
2023-05-17 15:02:53 +0000

    update

--- pwsh-AD/pwsh-AD.psm1
+++ pwsh-AD/pwsh-AD.psm1
@@ -3,30 +3,30 @@
 #requires -RunAsAdministrator
 
 #region::CarregarFuncoes
-Write-Verbose 'A carregar as funções privadas'
-. $PSScriptRoot\src\private\Copy-DadosAD.ps1
-. $PSScriptRoot\src\private\Edit-AllPunctuation.ps1
-. $PSScriptRoot\src\private\Enable-TLS12.ps1
-. $PSScriptRoot\src\private\Export-ADUsersCsv.ps1
-. $PSScriptRoot\src\private\Get-LatestItem.ps1
-. $PSScriptRoot\src\private\Set-LogPath.ps1
-. $PSScriptRoot\src\private\Get-RandomPassword.ps1
-. $PSScriptRoot\src\private\New-EduADUser.ps1
-. $PSScriptRoot\src\private\Update-SrcVMs.ps1
+Write-Verbose 'Source private functions'
+. $PSScriptRoot/private/Copy-ADCsvExport.ps1
+. $PSScriptRoot/src/private/Enable-TLS12.ps1
+. $PSScriptRoot/src/private/Export-ADUsersCsv.ps1
+. $PSScriptRoot/src/private/Get-LatestItem.ps1
+. $PSScriptRoot/src/private/Get-RandomPassword.ps1
+. $PSScriptRoot/src/private/Remove-AllPunctuation.ps1
+. $PSScriptRoot/src/private/Set-ADUserPasswordExpiration.ps1
+. $PSScriptRoot/src/private/Set-LogPath.ps1
+. $PSScriptRoot/src/private/Update-SrcVMs.ps1
 
-Write-Verbose 'A carregar as funções públicas'
-. $PSScriptRoot\src\public\Add-ConditionalGroupMember.ps1
-. $PSScriptRoot\src\public\Get-ADUserbyOU.ps1
-. $PSScriptRoot\src\public\Get-LatestCreatedObjects.ps1
-. $PSScriptRoot\src\public\Import-UsersToCreateFromCSV.ps1
-. $PSScriptRoot\src\public\Invoke-ADExportSGE.ps1
-. $PSScriptRoot\src\public\Invoke-ADTasks.ps1
-. $PSScriptRoot\src\public\Move-ADObjectOU.ps1
-. $PSScriptRoot\src\public\New-ADLogon.ps1
-. $PSScriptRoot\src\public\New-EduADMUser.ps1
-. $PSScriptRoot\src\public\New-EduUser.ps1
-. $PSScriptRoot\src\public\Set-CustomAttributes.ps1
-. $PSScriptRoot\src\public\Test-ADComputerNamingRules.ps1
+Write-Verbose 'Source public functions'
+. $PSScriptRoot/src/public/Add-ConditionalGroupMember.ps1
+. $PSScriptRoot/src/public/Get-ADUserbyOU.ps1
+. $PSScriptRoot/src/public/Get-LatestCreatedObjects.ps1
+. $PSScriptRoot/src/public/Import-UsersToCreateFromCSV.ps1
+. $PSScriptRoot/src/public/Invoke-ADExport.ps1
+. $PSScriptRoot/src/public/Invoke-ADTasks.ps1
+. $PSScriptRoot/src/public/Move-ADObjectOU.ps1
+. $PSScriptRoot/src/public/New-ADCustomUser.ps1
+. $PSScriptRoot/src/public/New-AdLogon.ps1
+. $PSScriptRoot/src/public/Set-CustomAttributes.ps1
+. $PSScriptRoot/src/public/Test-ADComputerNamingRules.ps1
+. $PSScriptRoot/src/public/Test-ADUserAttributes.ps1
 #endregion::CarregarFuncoes
 
 #region::SetAutomaticVariablesPreference
@@ -40,11 +40,6 @@ Enable-TLS12
 
 # region:: Imports
 Import-Module ActiveDirectory
-
-#region::SetDomainController <when running remotely>
-# Write-Verbose 'Setting DomainController to E000DCSD-VM'
-# $global:DC_A = 'S0204DCA.edu.azores.gov.local'
-#endregion::SetDomainController
 
 #region::SetLogFilePath
 $global:LogPath = 'c:\logs\exceptions'
